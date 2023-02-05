8c012fe3434d648d30ec0f1d3327d75aa56cbc59
m4ul3r <m4ul3r@protonmail.com>
2023-02-05 09:57:52 -0500

    add color to markdown output

--- binja_rop/rop.py
+++ binja_rop/rop.py
@@ -5,7 +5,7 @@ rop.py: calculate rop gadgets contained in the executable sections of binaries
 from operator import itemgetter
 from binaryninja import *
 
-_PREV_BYTE_SIZE = 9
+_PREV_BYTE_SIZE = 32
 
 _RET_INSTRS = {"retn": [b"\xc3", b"\xf2\xc3"], "retf": [b"\xcb"]}
 
@@ -14,7 +14,7 @@ def _disas_all_instrs(bv: BinaryView, start_addr, ret_addr: int) -> list[str]:
     """
     disassemble all instructions in chunk
     """
-    instructions = []
+    instructions: list[str] = []
     curr_addr = start_addr
     while curr_addr < ret_addr:
         instr = bv.get_disassembly(curr_addr)
@@ -35,46 +35,43 @@ def _disas_all_instrs(bv: BinaryView, start_addr, ret_addr: int) -> list[str]:
     return instructions
 
 
-def _calculate_gadget_from_ret(
-    bv: BinaryView, gadgets: dict[int, str], baseaddr, ret_addr: int
-):
+def _calculate_gadget_from_ret(bv: BinaryView, gadgets: dict[int, str], ret_addr: int):
     """
     decrement index from ret ins and calculate gadgets
     """
     ret_instr = bv.get_disassembly(ret_addr)
-    for i in range(1, _PREV_BYTE_SIZE):
+    for i in range(0, _PREV_BYTE_SIZE + 1):
         instructions = _disas_all_instrs(bv, ret_addr - i, ret_addr)
         if instructions is None:
             continue
         gadget_str = ""
         for instr in instructions:
-            print(instr)
-            print(type(instr))
             gadget_str += f"{instr} ; "
-        gadget_rva = ret_addr - i - baseaddr
+        gadget_rva = ret_addr - i - bv.start
         gadgets[gadget_rva] = f"{gadget_str}{ret_instr}"
     return gadgets
 
 
-def _find_gadgets_in_data(bv: BinaryView, baseaddr: int, section: Section) -> dict:
+def _find_gadgets_in_data(
+    bv: BinaryView, insts: tuple[list[str], int]
+) -> dict[int, str]:
     """
     find ret instructions and spawn a thread to calculate gadgets
     """
-    gadgets = dict()
+    gadgets: dict[int, str] = dict()
     for _, bytecodes in _RET_INSTRS.items():
         for bytecode in bytecodes:
-            next_start = section.start
+            next_start = insts[0][1]
             next_ret_addr = 0
-            while next_start < section.end:
+            while next_start < insts[-1][1]:
                 next_ret_addr = bv.find_next_data(next_start, bytecode)
-                if next_ret_addr == None:
+                if next_ret_addr is None:
                     break
 
                 # TODO thread this
-                gadgets = _calculate_gadget_from_ret(
-                    bv, gadgets, baseaddr, next_ret_addr
-                )
+                gadgets = _calculate_gadget_from_ret(bv, gadgets, next_ret_addr)
                 next_start = next_ret_addr + len(bytecode)
+                # [y[1] for y in ins].index(4199412)
 
     return gadgets
 
@@ -85,42 +82,47 @@ def _generate_output(bv: BinaryView, gadgets: dict[int, str], title: str):
     """
     markdown = f"rop gadgets found for {bv.file.filename}\n\n"
     body = ""
+    pop_gadgets = ""
+    mov_gadgets = ""
+    all_gadgets = ""
     found = []
+    gadgets = dict(sorted(gadgets.items()))
+    print(gadgets)
     for addr, gadget in sorted(gadgets.items(), key=itemgetter(1)):
         if gadget not in found:
             gadget_str = " ".join(gadget.split())
             addr = addr + bv.start  # make sure addrs are correct
-            # if "pop" in gadget_str:
-            # gadget_str = gadget_str.replace("pop", '<span style="color: red;">pop</span>')
-            body += f"[0x{addr:016x}](binaryninja://?expr={addr:08x}) \t```{gadget_str}```\n\n"
+            if "pop" in gadget_str:
+                gadget_str = gadget_str.replace(
+                    "pop", '<span style="color: red;">pop</span>'
+                )
+
+            f_gadget_str = (
+                f"[0x{addr:016x}](binaryninja://?expr={addr:08x})  \t{gadget_str}\n\n"
+            )
+            all_gadgets += f_gadget_str
+
+            if "pop" in f_gadget_str:
+                pop_gadgets += f_gadget_str
+            if "mov" in f_gadget_str:
+                mov_gadgets += f_gadget_str
+
             found.append(gadget)
     markdown += f"[+] found {len(found)} gadgets\n***\n"
+
+    markdown += "[+] pop gadgets\n\n"
+    markdown += pop_gadgets
+    markdown += "***\n\n"
+
+    markdown += "[+] mov gadgets\n\n"
+    markdown += mov_gadgets
+    markdown += "***\n\n"
+
+    markdown += "[+] all gadgets\n\n"
+    body += all_gadgets
+
     markdown += body
     bv.show_markdown_report(title, markdown)
-
-
-# def _generate_html(bv, gadgets, title):
-#     """
-#     display rop gadgets
-#     """
-#     markdown = ""
-#     # markdown += '<span style="white-space: pre-wrap;">'
-#     markdown += "<head><style> tab {display: inline-block; margin-left: 500px;}"
-#     markdown += "</style></head>"
-#     found = []
-#     for addr, gadget in sorted(gadgets.items(), key=itemgetter(1)):
-#         if gadget not in found:
-#             gadget_str = " ".join(gadget.split())
-#             addr = addr + bv.start  # make sure addrs are correct
-#             if "pop" in gadget_str:
-#                 gadget_str = gadget_str.replace(
-#                     "pop", '<span style="color: red;">pop</span>'
-#                 )
-#             markdown += '<nobr><p style="display: line;"><a href=binaryninja://?expr={:08x}>0x{:016x}</a><tab><code>{}</code></p></nobr>'.format(
-#                 addr, addr, gadget_str
-#             )
-#             found.append(gadget)
-#     bv.show_html_report(title, markdown)
 
 
 class ROPSearch(BackgroundTaskThread):
@@ -145,12 +147,9 @@ class ROPSearch(BackgroundTaskThread):
         if not self.view.executable:
             return
 
-        baseaddr = self.view.segments[0].start
-        # get more than just `.text` section
-        # bv.is_offset_executable(addr:int)
-        # section = self.view.get_section_by_name(".text")
-
-        gadgets = _find_gadgets_in_data(self.view, baseaddr, section)
+        # get instructions rather than sections
+        instructions = [i for i in self.view.instructions]
+        gadgets = _find_gadgets_in_data(self.view, instructions)
 
         if gadgets != {}:
             _generate_output(self.view, gadgets, "rop gadgets")
