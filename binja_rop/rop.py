18bbc9e86bef8b0f843fa40e7713b625718ad09d
m4ul3r <m4ul3r@protonmail.com>
2023-02-05 07:39:03 -0500

    fix bugs, linting, formatting

--- binja_rop/rop.py
+++ binja_rop/rop.py
@@ -2,21 +2,18 @@
 rop.py: calculate rop gadgets contained in the executable sections of binaries
 """
 
-from binaryninja import *
 from operator import itemgetter
-import binascii
-import argparse
+from binaryninja import *
 
 _PREV_BYTE_SIZE = 9
 
 _RET_INSTRS = {"retn": [b"\xc3", b"\xf2\xc3"], "retf": [b"\xcb"]}
 
 
-def _disas_all_instrs(bv, start_addr, ret_addr):
+def _disas_all_instrs(bv: BinaryView, start_addr, ret_addr: int) -> list[str]:
     """
     disassemble all instructions in chunk
     """
-    global _RET_INSTRS
     instructions = []
     curr_addr = start_addr
     while curr_addr < ret_addr:
@@ -38,33 +35,33 @@ def _disas_all_instrs(bv, start_addr, ret_addr):
     return instructions
 
 
-def _calculate_gadget_from_ret(bv, gadgets, baseaddr, ret_addr):
+def _calculate_gadget_from_ret(
+    bv: BinaryView, gadgets: dict[int, str], baseaddr, ret_addr: int
+):
     """
     decrement index from ret ins and calculate gadgets
     """
-    global _RET_INSTRS
     ret_instr = bv.get_disassembly(ret_addr)
-    print(f"A{gadgets}")
-    print(f"gadgets: {gadgets}")
     for i in range(1, _PREV_BYTE_SIZE):
         instructions = _disas_all_instrs(bv, ret_addr - i, ret_addr)
-        if instructions == None:
+        if instructions is None:
             continue
         gadget_str = ""
         for instr in instructions:
+            print(instr)
+            print(type(instr))
             gadget_str += f"{instr} ; "
         gadget_rva = ret_addr - i - baseaddr
         gadgets[gadget_rva] = f"{gadget_str}{ret_instr}"
     return gadgets
 
 
-def _find_gadgets_in_data(bv, baseaddr, section):
+def _find_gadgets_in_data(bv: BinaryView, baseaddr: int, section: Section) -> dict:
     """
     find ret instructions and spawn a thread to calculate gadgets
     """
-    global _RET_INSTRS
     gadgets = dict()
-    for ret_instr, bytecodes in _RET_INSTRS.items():
+    for _, bytecodes in _RET_INSTRS.items():
         for bytecode in bytecodes:
             next_start = section.start
             next_ret_addr = 0
@@ -82,47 +79,48 @@ def _find_gadgets_in_data(bv, baseaddr, section):
     return gadgets
 
 
-def _generate_output(bv, gadgets, title):
+def _generate_output(bv: BinaryView, gadgets: dict[int, str], title: str):
     """
     display rop gadgets
     """
-    markdown = ""
+    markdown = f"rop gadgets found for {bv.file.filename}\n\n"
+    body = ""
     found = []
     for addr, gadget in sorted(gadgets.items(), key=itemgetter(1)):
         if gadget not in found:
             gadget_str = " ".join(gadget.split())
+            addr = addr + bv.start  # make sure addrs are correct
             # if "pop" in gadget_str:
             # gadget_str = gadget_str.replace("pop", '<span style="color: red;">pop</span>')
-            markdown += "[0x{:016x}](binaryninja://?expr={:08x}) \t```{}```\n\n".format(
-                addr, addr, gadget_str
-            )
+            body += f"[0x{addr:016x}](binaryninja://?expr={addr:08x}) \t```{gadget_str}```\n\n"
             found.append(gadget)
-            print(markdown)
+    markdown += f"[+] found {len(found)} gadgets\n***\n"
+    markdown += body
     bv.show_markdown_report(title, markdown)
 
 
-def _generate_html(bv, gadgets, title):
-    """
-    display rop gadgets
-    """
-    markdown = ""
-    # markdown += '<span style="white-space: pre-wrap;">'
-    markdown += "<head><style> tab {display: inline-block; margin-left: 500px;}"
-    markdown += "</style></head>"
-    found = []
-    for addr, gadget in sorted(gadgets.items(), key=itemgetter(1)):
-        if gadget not in found:
-            gadget_str = " ".join(gadget.split())
-            if "pop" in gadget_str:
-                gadget_str = gadget_str.replace(
-                    "pop", '<span style="color: red;">pop</span>'
-                )
-            markdown += '<nobr><p style="display: line;"><a href=binaryninja://?expr={:08x}>0x{:016x}</a><tab><code>{}</code></p></nobr>'.format(
-                addr, addr, gadget_str
-            )
-            found.append(gadget)
-            print(markdown)
-    bv.show_html_report(title, markdown)
+# def _generate_html(bv, gadgets, title):
+#     """
+#     display rop gadgets
+#     """
+#     markdown = ""
+#     # markdown += '<span style="white-space: pre-wrap;">'
+#     markdown += "<head><style> tab {display: inline-block; margin-left: 500px;}"
+#     markdown += "</style></head>"
+#     found = []
+#     for addr, gadget in sorted(gadgets.items(), key=itemgetter(1)):
+#         if gadget not in found:
+#             gadget_str = " ".join(gadget.split())
+#             addr = addr + bv.start  # make sure addrs are correct
+#             if "pop" in gadget_str:
+#                 gadget_str = gadget_str.replace(
+#                     "pop", '<span style="color: red;">pop</span>'
+#                 )
+#             markdown += '<nobr><p style="display: line;"><a href=binaryninja://?expr={:08x}>0x{:016x}</a><tab><code>{}</code></p></nobr>'.format(
+#                 addr, addr, gadget_str
+#             )
+#             found.append(gadget)
+#     bv.show_html_report(title, markdown)
 
 
 class ROPSearch(BackgroundTaskThread):
@@ -130,12 +128,14 @@ class ROPSearch(BackgroundTaskThread):
     class that assists in location rop gadgets in executable code segments
     """
 
-    def __init__(self, view):
+    def __init__(self, view: BinaryView):
         BackgroundTaskThread.__init__(self, "", True)
         self.view = view
         self.gadgets = {}
         self.progress = "[+] binja_rop: searching for rop gadgets"
         self.threads = []
+        self.endianness = self.view.perform_get_default_endianness()
+        self.arch = self.view.arch
         self.ret_instrs = _RET_INSTRS
 
     def run(self):
@@ -146,7 +146,10 @@ class ROPSearch(BackgroundTaskThread):
             return
 
         baseaddr = self.view.segments[0].start
-        section = self.view.get_section_by_name(".text")
+        # get more than just `.text` section
+        # bv.is_offset_executable(addr:int)
+        # section = self.view.get_section_by_name(".text")
+
         gadgets = _find_gadgets_in_data(self.view, baseaddr, section)
 
         if gadgets != {}:
